d05457e155bc870978a359894e706b4b6a3d3bd1
saksham9140 <sakshamyadav9140@gmail.com>
2026-06-20 00:08:16 +0530

    approval images fix

--- src/Components/Approval/Approval.jsx
+++ src/Components/Approval/Approval.jsx
@@ -1,24 +1,35 @@
 import './Approval.css';
 
+import logo1 from './logo/1.jpg';
+import logo2 from './logo/2.jpg';
+import logo3 from './logo/3.jpg';
+import logo4 from './logo/4.jpg';
+import logo5 from './logo/5.jpg';
+import logo6 from './logo/6.jpg';
+import logo7 from './logo/7.jpg';
+import logo8 from './logo/8.jpg';
+import logo9 from './logo/9.jpg';
+import logo10 from './logo/10.jpg';
+
 function App() {
-    return (
-      <div>
-        <div className="logos">
-    <div class="logos-slide">
-        <img src="src/Components/Approval/logo/1.jpg" />
-        <img src="src/Components/Approval/logo/2.jpg" />
-        <img src="src/Components/Approval/logo/3.jpg" />
-        <img src="src/Components/Approval/logo/4.jpg" />
-        <img src="src/Components/Approval/logo/5.jpg" />
-        <img src="src/Components/Approval/logo/6.jpg" />
-        <img src="src/Components/Approval/logo/7.jpg" />
-        <img src="src/Components/Approval/logo/8.jpg" />
-        <img src="src/Components/Approval/logo/9.jpg" />
-        <img src="src/Components/Approval/logo/10.jpg" />
-    </div>
+  return (
+    <div>
+      <div className="logos">
+        <div className="logos-slide">
+          <img src={logo1} alt="Approval 1" />
+          <img src={logo2} alt="Approval 2" />
+          <img src={logo3} alt="Approval 3" />
+          <img src={logo4} alt="Approval 4" />
+          <img src={logo5} alt="Approval 5" />
+          <img src={logo6} alt="Approval 6" />
+          <img src={logo7} alt="Approval 7" />
+          <img src={logo8} alt="Approval 8" />
+          <img src={logo9} alt="Approval 9" />
+          <img src={logo10} alt="Approval 10" />
         </div>
       </div>
-    );
-  }
-  
-  export default App;
+    </div>
+  );
+}
+
+export default App;
