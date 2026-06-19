ff79614d303157c54b575de1caf22ad36b74e8d0
saksham9140 <sakshamyadav9140@gmail.com>
2026-06-20 00:01:21 +0530

    images fix

--- src/Components/Collab/Collab.jsx
+++ src/Components/Collab/Collab.jsx
@@ -1,30 +1,39 @@
-
 import './Collab.css';
 
-
+import bukhara from './univ/bukhara.jpg';
+import burev from './univ/burev.jpg';
+import erzincan from './univ/erzincan.jpg';
+import hellenic from './univ/hellenic.jpg';
+import moscow from './univ/Moscow-Polytechnic-University.jpg';
+import pavlodar from './univ/pavlodar.jpg';
+import sunway from './univ/sunway.jpg';
+import swissInsti from './univ/swiss insti.jpg';
+import thumbay from './univ/Thumbay-University-Dubai.jpg';
+import unitar from './univ/unitar.jpg';
+import utm from './univ/utm.jpg';
+import yaroslav from './univ/yaroslav.jpg';
 
 function App() {
   return (
     <div>
       <div className="logos">
         <div className="logos-slide">
-          <img src="src/Components/Collab/univ/bukhara.jpg" alt="Bukhara University" />
-          <img src="src/Components/Collab/univ/burev.jpg" alt="Burev University" />
-          <img src="src/Components/Collab/univ/erzincan.jpg" alt="Erzincan University" />
-          <img src="src/Components/Collab/univ/hellenic.jpg" alt="Hellenic University" />
-          <img src="src/Components/Collab/univ/Moscow-Polytechnic-University.jpg" alt="Moscow Polytechnic University" />
-          <img src="src/Components/Collab/univ/pavlodar.jpg" alt="Pavlodar University" />
-          <img src="src/Components/Collab/univ/sunway.jpg" alt="Sunway University" />
-          <img src="src/Components/Collab/univ/swiss insti.jpg" alt="Swiss Institute" />
-          <img src="src/Components/Collab\univ/Thumbay-University-Dubai.jpg" alt="Thumbay University Dubai" />
-          <img src="src/Components/Collab/univ/unitar.jpg" alt="Unitar" />
-          <img src="src/Components/Collab/univ/utm.jpg" alt="Utm" />
-          <img src="src/Components/Collab/univ/yaroslav.jpg" alt="Yaroslav" />
+          <img src={bukhara} alt="Bukhara University" />
+          <img src={burev} alt="Burev University" />
+          <img src={erzincan} alt="Erzincan University" />
+          <img src={hellenic} alt="Hellenic University" />
+          <img src={moscow} alt="Moscow Polytechnic University" />
+          <img src={pavlodar} alt="Pavlodar University" />
+          <img src={sunway} alt="Sunway University" />
+          <img src={swissInsti} alt="Swiss Institute" />
+          <img src={thumbay} alt="Thumbay University Dubai" />
+          <img src={unitar} alt="Unitar" />
+          <img src={utm} alt="UTM" />
+          <img src={yaroslav} alt="Yaroslav" />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
- 
