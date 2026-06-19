36da7647f12f8f3d0163b252e9ae13095253994d
saksham9140 <sakshamyadav9140@gmail.com>
2026-06-20 00:05:25 +0530

    hiring images fix

--- src/Components/Hiring/Hiring.jsx
+++ src/Components/Hiring/Hiring.jsx
@@ -1,35 +1,57 @@
 import './Hiring.css';
 
+import apolloLogo from './logo/apollo-hospitals-logo.png';
+import birlaSoftLogo from './logo/birla-soft-logo.png';
+import byjusLogo from './logo/BYJUS_NEW_LOGO.png';
+import capgeminiLogo from './logo/Capgemini-Logo.wine.png';
+import samsungLogo from './logo/classic-samsung-logo-png-0.png';
+import cognizantLogo from './logo/Cognizant_Technology_Solutions_Corp_Logo_old.png';
+import flipkartLogo from './logo/flipkart-logo.png';
+import fortisLogo from './logo/Fortis-Emblem.png';
+import googleLogo from './logo/Google-Logo-PNG-Free-Image.png';
+import hclLogo from './logo/HCL_Technologies_Logo.png';
+import hondaLogo from './logo/honda-logo-7-png-3072-2416-logos-pinterest-honda-and-motorcycle-companies-3072.png';
+import wiproLogo from './logo/wipro-wipro-logo-1683541.png';
+import infoEdgeLogo from './logo/info-edge-logo-1A0FB447C4-seeklogo.com.png';
+import infosysLogo from './logo/infosys-navigate-your-next-logo-natural-vert-nav-copy.png';
+import jaypeeLogo from './logo/jaypee.png';
+import logoBl from './logo/logo-bl.png';
+import maxLogo from './logo/max.png';
+import niimsLogo from './logo/niims-logo-1D5BF93140-seeklogo.com.png';
+import synergyLogo from './logo/synergy.png';
+import tcsLogo from './logo/Tata-Consultancy-Services-TCS-Logo-Vector.svg-.png';
+import vimhansLogo from './logo/vimhans_nayati.png';
+
 function App() {
-    return (
-      <div>
-        <div className="com-logos">
-    <div class="com-logos-slide">
-        <img src="src/Components/Hiring/logo/apollo-hospitals-logo.png" />
-        <img src="src/Components/Hiring/logo/birla-soft-logo.png" />
-        <img src="src/Components/Hiring/logo/BYJUS_NEW_LOGO.png" />
-        <img src="src/Components/Hiring/logo/Capgemini-Logo.wine.png" />
-        <img src="src/Components/Hiring/logo/classic-samsung-logo-png-0.png" />
-        <img src="src/Components/Hiring/logo/Cognizant_Technology_Solutions_Corp_Logo_old.png" />
-        <img src="src/Components/Hiring/logo/flipkart-logo.png" />
-        <img src="src/Components/Hiring/logo/Fortis-Emblem.png" />
-        <img src="src/Components/Hiring/logo/Google-Logo-PNG-Free-Image.png" />
-        <img src="src/Components/Hiring/logo/HCL_Technologies_Logo.png" />
-        <img src="src/Components/Hiring/logo/honda-logo-7-png-3072-2416-logos-pinterest-honda-and-motorcycle-companies-3072.png" />
-        <img src="src/Components/Hiring/logo/wipro-wipro-logo-1683541.png" />
-        <img src="src/Components/Hiring/logo/info-edge-logo-1A0FB447C4-seeklogo.com.png" />
-        <img src="src/Components/Hiring/logo/infosys-navigate-your-next-logo-natural-vert-nav-copy.png" />
-        <img src="src/Components/Hiring/logo/jaypee.png" />
-        <img src="src/Components/Hiring/logo/logo-bl.png" />
-        <img src="src/Components/Hiring/logo/max.png" />
-        <img src="src/Components/Hiring/logo/niims-logo-1D5BF93140-seeklogo.com.png" />
-        <img src="src/Components/Hiring/logo/synergy.png" />
-        <img src="src/Components/Hiring/logo/Tata-Consultancy-Services-TCS-Logo-Vector.svg-.png" />
-        <img src="src/Components/Hiring/logo/vimhans_nayati.png" />
-    </div>
+  return (
+    <div>
+      <div className="com-logos">
+        <div className="com-logos-slide">
+          <img src={apolloLogo} alt="Apollo Hospitals" />
+          <img src={birlaSoftLogo} alt="Birla Soft" />
+          <img src={byjusLogo} alt="BYJU'S" />
+          <img src={capgeminiLogo} alt="Capgemini" />
+          <img src={samsungLogo} alt="Samsung" />
+          <img src={cognizantLogo} alt="Cognizant" />
+          <img src={flipkartLogo} alt="Flipkart" />
+          <img src={fortisLogo} alt="Fortis" />
+          <img src={googleLogo} alt="Google" />
+          <img src={hclLogo} alt="HCL" />
+          <img src={hondaLogo} alt="Honda" />
+          <img src={wiproLogo} alt="Wipro" />
+          <img src={infoEdgeLogo} alt="Info Edge" />
+          <img src={infosysLogo} alt="Infosys" />
+          <img src={jaypeeLogo} alt="Jaypee" />
+          <img src={logoBl} alt="Logo BL" />
+          <img src={maxLogo} alt="Max" />
+          <img src={niimsLogo} alt="NIIMS" />
+          <img src={synergyLogo} alt="Synergy" />
+          <img src={tcsLogo} alt="TCS" />
+          <img src={vimhansLogo} alt="Vimhans Nayati" />
         </div>
       </div>
-    );
-  }
-  
-  export default App;
+    </div>
+  );
+}
+
+export default App;
